78bf799e147e2c2a3e5ee2a796bd3bd8dfa82034
Harshit Yadav <2105891@kiit.ac.in>
2024-07-02 21:20:42 +0530

    feat: There ways of building form control

--- src/app/pages/reactive-forms/reactive-forms.component.ts
+++ src/app/pages/reactive-forms/reactive-forms.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './reactive-forms.component.html',
-  styleUrl: './reactive-forms.component.scss'
+  styleUrl: './reactive-forms.component.scss',
 })
 export class ReactiveFormsComponent {
-  
+  regForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+    this.regForm = new FormGroup({
+      fname: new FormControl(''),
+      lname: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  ngOnInit() {
+    // 1st way of creating instance of FormGroup class
+    // this.regForm = new FormGroup({
+    //   fname: new FormControl(''),
+    //   lname: new FormControl(''),
+    //   email: new FormControl(''),
+    //   password: new FormControl(''),
+    // });
+
+    // 2nd way
+    this.regForm = this._fb.group({
+      fname: new FormControl(''),
+      lname: new FormControl(''),
+      email: new FormControl('example@mail.com'),
+      password: new FormControl('******'),
+    });
+
+    // 3rd way
+    this.regForm = this._fb.group({
+      fname: [],
+      lname: [],
+      email: [],
+      password: [],
+    });
+  }
+
+  register(formData: FormGroup) {
+    // console.log(formData);
+
+    // 2nd way of getting values
+    console.log(this.regForm);
+    console.log(this.regForm.value);
+  }
 }
